7ee2849162988d0b771b42bc791fb8596f1362b9
Sarthak <sarthak.8858@gmail.com>
2023-02-05 17:17:58 +0530

    Crud application completed

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sarthak-19/go-crud-postgres/controllers"
+	"github.com/sarthak-19/go-crud-postgres/database"
+)
+
+func main() {
+	fmt.Println("Starting application ...")
+	database.DatabaseConnection()
+
+	r := gin.Default()
+	r.GET("/books/:id", controllers.ReadBook)
+	r.GET("/books", controllers.ReadBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.PUT("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+	r.Run(":5000")
+}
